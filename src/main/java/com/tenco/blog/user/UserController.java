78a910fc19e4ed5927589ff6026c76363fafd0f1
suuu1021 <whs5758@naver.com>
2025-06-25 17:35:13 +0900

    코드 리팩토링

--- src/main/java/com/tenco/blog/user/UserController.java
+++ src/main/java/com/tenco/blog/user/UserController.java
@@ -1,142 +1,117 @@
 package com.tenco.blog.user;
 
+import com.tenco.blog._core.errors.exception.Exception400;
+import com.tenco.blog._core.errors.exception.Exception401;
+import com.tenco.blog.board.BoardController;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@RequiredArgsConstructor // DI 처리
+@RequiredArgsConstructor
 @Controller
 public class UserController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
     private final UserRepository userRepository;
-    // httpSession <--- 세션 메모리에 접근을 할 수 있음
     private final HttpSession httpSession;
 
-    // 회원 정보 수정 화면
-    // 주소 설계 : http://localhost:8080/user/update-form
     @GetMapping("/user/update-form")
     public String updateForm(HttpServletRequest request, HttpSession session) {
-        // 1. 로그인 체크
+
+        log.info("회원 정보 수정 폼 요청");
+
         User sessionUser = (User) session.getAttribute("sessionUser");
         if (sessionUser == null) {
-            return "redirect:/login-form";
+            throw new Exception401("로그인이 필요한 서비스 입니다");
         }
         request.setAttribute("user", sessionUser);
         return "user/update-form";
     }
 
-    // 회원 정보 수정 액션 처리
+
     @PostMapping("/user/update")
     public String update(UserRequest.UpdateDTO reqDTO,
                          HttpSession session, HttpServletRequest request) {
+
+        log.info("회원 정보 수정 요청");
+
         User sessionUser = (User) session.getAttribute("sessionUser");
         if (sessionUser == null) {
-            return "redirect:/login-form";
+            throw new Exception401("로그인이 필요한 서비스 입니다");
         }
-        // 데이터 유효성 검사 처리
         reqDTO.validate();
-        // 회원 정보 수정은 권한 체크가 필요 없다 (세션에서 정보를 가져오기 때문)
         User updateUser = userRepository.updateById(sessionUser.getId(), reqDTO);
-        // 세션 동기화
         session.setAttribute("sessionUser", updateUser);
-        // 다시 회원 정보 보기 화면 요청 - GET 요청
         return "redirect:/user/update-form";
     }
 
-    /**
-     * 회원 가입 화면 요청
-     *
-     * @return join-form.mustache
-     */
+
     @GetMapping("/join-form")
     public String joinForm() {
+
+        log.info("회원 가입 폼 요청");
+
         return "user/join-form";
     }
 
-    // 회원 가입 액션 처리
+
     @PostMapping("/join")
     public String join(UserRequest.JoinDTO joinDTO, HttpServletRequest request) {
-        System.out.println("=== 회원가입 요청 ===");
-        System.out.println("사용자 명: " + joinDTO.getUsername());
-        System.out.println("사용자 이메일: " + joinDTO.getEmail());
 
-        try {
-            // 1. 입력된 데이터 검증 (유효성 검사)
-            joinDTO.validate();
-            // 2. 사용자 명 중복 체크
-            User exsistUser = userRepository.findByUsername(joinDTO.getUsername());
-            if (exsistUser != null) {
-                throw new IllegalArgumentException("이미 존재하는 사용자명 입니다" + joinDTO.getUsername());
-            }
-            // 3. DTO를 User Object로 변환
-            User user = joinDTO.toEntity();
-            // 4. User Object를 영속화 처리
-            userRepository.save(user);
-            return "redirect:/login-form";
-        } catch (Exception e) {
-            // 검증 실패 시 보통 에러 메세지와 함께 다시 폼에 전달
-            request.setAttribute("errorMessage", "잘못된 요청입니다");
-            return "user/join-form";
+        log.info("회원 가입 기능 요청");
+        log.info("사용자 명: {} ", joinDTO.getUsername());
+        log.info("사용자 이메일: {} ", joinDTO.getEmail());
+
+        joinDTO.validate();
+
+        User exsistUser = userRepository.findByUsername(joinDTO.getUsername());
+        if (exsistUser != null) {
+            throw new Exception400("이미 존재하는 사용자명 입니다" + joinDTO.getUsername());
         }
+        User user = joinDTO.toEntity();
+        userRepository.save(user);
+        return "redirect:/login-form";
     }
 
-    /**
-     * 로그인 화면 요청
-     *
-     * @return login-form
-     */
+
     @GetMapping("/login-form")
     public String loginForm() {
-        // 반환값이 뷰(파일) 이름이 됨 (뷰 리졸버가 실제 파일 경로를 찾아 감)
+
+        log.info("로그인 폼 요청");
+
         return "user/login-form";
     }
 
-    // 로그인 액션 처리
-    // 자원의 요청은 GET 방식이다. 단, 로그인 요청은 예외
-    // 보안상의 이유 (주소창에 username,password가 띄워짐, 히스토리에 저장됨)
 
-    // DTO 패턴 활용
-    // 1. 입력 데이터 검증
-    // 2. 사용자명과 비밀번호를 DB에 접근해서 조회
-    // 3. 로그인 성공/실패 처리
-    // 4. 로그인 성공 -> 서버 측 메모리에 사용자 정보 저장
-    // 5. 메인 화면으로 리다이렉트
     @PostMapping("/login")
     public String login(UserRequest.LoginDTO loginDTO) {
-        System.out.println("=== 로그인 시도 ===");
-        System.out.println("사용자 명 : " + loginDTO.getUsername());
 
-        try {
-            // 1.
-            loginDTO.validate();
-            // 2.
-            User user = userRepository.findByUsernameAndPassword(loginDTO.getUsername(), loginDTO.getPassword());
-            // 3. 로그인 실패 : 일치된 사용자 없음
-            if (user == null) {
-                throw new IllegalArgumentException("사용자 명 또는 비밀번호가 일치하지 않습니다.");
-            }
-            // 4. 로그인 성공
-            httpSession.setAttribute("sessionUser", user);
-            // 5. 로그인 성공 후 리스트 페이지 이동
-            return "redirect:/";
+        log.info("로그인 기능 요청");
+        log.info("사용자 명 : {} ", loginDTO.getUsername());
 
-        } catch (Exception e) {
-            // 필요하다면 에러 메세지 생성해서 내려 보내기
-            return "user/login-form";
+        loginDTO.validate();
+
+        User user = userRepository.findByUsernameAndPassword(loginDTO.getUsername(), loginDTO.getPassword());
+        if (user == null) {
+            throw new Exception400("사용자 명 또는 비밀번호가 일치하지 않습니다.");
         }
+        httpSession.setAttribute("sessionUser", user);
+        return "redirect:/";
     }
 
-    // 로그아웃 처리
+
     @GetMapping("/logout")
     public String logout() {
-        // 세션 무효화
+
+        log.info("로그아웃");
+
         httpSession.invalidate();
-        // "redirect: " 스프링에서 접두사를 사용하면 다른 URL로 리다이렉트 됨
-        // 즉 리다이렉트 한다는 것은 뷰를 렌더링 하지 않고 브라우저가 재요청을
-        // 다시 하게끔 유도 한다.
         return "redirect:/";
     }
 
--- src/main/java/com/tenco/blog/user/UserRepository.java
+++ src/main/java/com/tenco/blog/user/UserRepository.java
@@ -1,28 +1,36 @@
 package com.tenco.blog.user;
 
+import com.tenco.blog._core.errors.exception.Exception400;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor // 생성자 의존 주입 - DI 처리
-@Repository // IoC 대상 + 싱글톤 패턴으로 관리 됨
+@RequiredArgsConstructor
+@Repository
 public class UserRepository {
 
+    private static final Logger log = LoggerFactory.getLogger(UserRepository.class);
     private final EntityManager em;
 
-    // 사용자 정보 조회(username, password)
 
-    /**
-     * 로그인 요청 기능 (사용자 정보 조회)
-     *
-     * @param username
-     * @param password
-     * @return 성공 시 User 엔티티 실패시 null 반환
-     */
+    @Transactional
+    public User updateById(Long id, UserRequest.UpdateDTO reqDTO) {
+
+        log.info("회원 정보 수정 시작 - id : {}", id);
+
+        User user = findById(id);
+        user.setPassword(reqDTO.getPassword());
+        return user;
+    }
+
+
     public User findByUsernameAndPassword(String username, String password) {
-        //JPQL
+
+        // 필요 하다면 직접 예외 처리 설정
         try {
             String jpql = "SELECT u FROM User u " +
                     "WHERE u.username = :username AND u.password = :password ";
@@ -31,34 +39,26 @@ public class UserRepository {
             typedQuery.setParameter("password", password);
             return typedQuery.getSingleResult();
         } catch (Exception e) {
-            // 일치하는 사용자가 없거나 에러 발생 시 null 반환
-            // 즉, 로그인 실패를 의미함
             return null;
         }
     }
 
-    /**
-     * 회원 정보 저장 처리
-     *
-     * @param user (비영속 상태)
-     * @return User 엔티티 반환
-     */
+
     @Transactional
     public User save(User user) {
-        // 매개변수에 들어오는 user Object는 비영속화 된 상태이다.
-        em.persist(user); // 영속성 컨텍스트에 user 객체를 관리하기 시작 함
-        // 트랜잭션 커밋 시점에 실제 INSERT 쿼리를 실행한다.
+
+        log.info("회원 정보 저장 시작");
+
+        em.persist(user);
         return user;
     }
 
-    // 사용자명 중복 체크용 조회 기능
-    public User findByUsername(String username) {
-        // WHERE username =?
-//        String jpql = " SELECT u FROM User u WHERE u.username = :username ";
-//        TypedQuery<User> typedQuery = em.createQuery(jpql, User.class);
-//        typedQuery.setParameter("username", username);
-//        return typedQuery.getSingleResult();
 
+    public User findByUsername(String username) {
+
+        log.info("중복 사용자 이름 조회");
+
+        // 필요 하다면 직접 예외 처리
         try {
             String jpql = " SELECT u FROM User u WHERE u.username = :username ";
             return em.createQuery(jpql, User.class)
@@ -67,24 +67,18 @@ public class UserRepository {
         } catch (Exception e) {
             return null;
         }
-
     }
 
+
     public User findById(Long id) {
+
+        log.info("사용자 조회 - id : {} " + id);
+
         User user = em.find(User.class, id);
         if (user == null) {
-            throw new RuntimeException("사용자를 찾을 수 없습니다.");
+            throw new Exception400("사용자를 찾을 수 없습니다.");
         }
         return user;
     }
 
-    @Transactional
-    public User updateById(Long id, UserRequest.UpdateDTO reqDTO) {
-        // 조회, 객체의 상태값 변경, 트랜잭션
-        User user = findById(id);
-        // 객체의 상태값을 행위를 통해서 변경
-        user.setPassword(reqDTO.getPassword());
-        // 수정된 영속 엔티티 반환 (세션 동기화 용)
-        return user;
-    }
 }
